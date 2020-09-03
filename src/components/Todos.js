62129a5abdee054aa8aee9ad5753ab0c4456fd4d
Kayla.duffy <kayla.duffy@whag18kayla.lan>
2020-09-02 20:05:14 -0500

    Added link between todo and app.js

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React from 'react'
 
 const Todos = ({todos}) => {
-    
-    const todoList = todos.length ? () : ()
+
+    const todoList = todos.length ? (
+        todos.map(todo => {
+            return (
+                <div className="collection-item">
+                    <span>{todo.content}</span>
+                </div>
+            )
+        })
+    ) : (
+        <p className="center">You have no todos</p> 
+    )
     return (
         <div className="todos collection">
-
+        
         </div>
-
     )
 }
