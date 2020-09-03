4088e1d43137c9213f39c809647edaae9d4a89e2
Kayla.duffy <kayla.duffy@whag18kayla.lan>
2020-09-02 19:54:31 -0500

    Started a todo list structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Todos from '../src/components/Todos';
+
 
 function App() {
+  state = {
+    todos: [
+      {id: 1, content: 'Buy groceries'},
+      {id: 2, content: 'Do laundry'}
+    ]
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="center pink-text">Todo's</h1>
+      <Todos todos={this.state.todos} />
     </div>
   );
 }
---
+++ src/components/Todos.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Todos = ({todos}) => {
+    
+    const todoList = todos.length ? () : ()
+    return (
+        <div className="todos collection">
+
+        </div>
+
+    )
+}
